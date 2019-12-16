95eeb8c50f6468c72340dcefa46e3e49730929e8
tsuruki <sigeru0216@gmail.com>
2019-12-16 16:54:19 +0900

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Folder;
+
+class TaskController extends Controller
+{
+    public function index() {
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
+    }
+}
